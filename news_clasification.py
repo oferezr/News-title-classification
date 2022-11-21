e8fdba98f9a42dc43afa2671eab2793057802507
oferezr <65119815+oferezr@users.noreply.github.com>
2022-11-21 22:09:52 +0200

    Making the probe better and adding more titles

--- news_clasification.py
+++ news_clasification.py
@@ -8,25 +8,39 @@ import json
 
 DATA_SOURCE = ['https://www.haaretz.com/srv/haaretz-latest-headlines',
                'https://www.israelhayom.co.il/rss.xml']
-DATA_PATH = "D:/הפקות ופרויקטים/Pythone projects/News_title_classification/data/dataset.json"
+DATA_PATH = "C:/Users/Ofer Ezrachi/Documents/Python Scripts/News_title_classification/data/dataset.json"
 
 
-def probe_website(url: str, clasification: int) -> Dict[str, int]:
+def probe_website(url: str, classification: int) -> Dict[str, int]:
+    """
+    This function will probe in a website to search for titles
+    :param url: the url of the website to probe in
+    :param classification: The classification to set to the title found
+    :return: Dictionary of the title to the classification
+    """
     http = urllib3.PoolManager()
     response = http.request('GET', url)
+    # Get string data with utf 8 encoding
     data_str = str(response.data.decode('utf-8'))
+    # Clean website errors of rss savings
     data_clean = data_str[
                  data_str.find('<rss'):data_str.find('</rss>') + 6].replace(
         '<iframe title=', '').replace('\\n', '\n')
+    # Get relevant data from the string
     data = xmltodict.parse(data_clean)
     data = data['rss']['channel']['item']
+    # Map the titles to the classification and return
+    return {item['title']: classification for item in data}
 
-    return {item['title']: clasification for item in data}
 
-
-def dump_to_file(data: Dict[str, int], path):
+def dump_to_file(data: Dict[str, int], path)->None:
+    """
+    This function will dump the data to file as json
+    :param data: data to dump
+    :param path: path to dump to
+    """
     with open(path, "w", encoding='utf8') as file:
-        json.dump(data, file, ensure_ascii=False)
+        json.dump(data, file, ensure_ascii=False,indent=2)
 
 
 def read_dump(path: str) -> Dict[str, int]:
@@ -46,5 +60,3 @@ def update_dataset(path: str):
 
 if __name__ == '__main__':
     update_dataset(DATA_PATH)
-    with open('./file.txt') as file:
-        file.write("Hello")
--- update_dataset.bat
+++ update_dataset.bat
@@ -1,2 +1,2 @@
-D:\WPy64-3771\python-3.7.7.amd64\python.exe "D:\הפקות ופרויקטים\Pythone projects\News_title_classification\news_clasification.py"
+D:\WPy64-3771\python-3.7.7.amd64\python.exe "C:\Users\Ofer Ezrachi\Documents\Python Scripts\News_title_classification\news_clasification.py"
 pause
