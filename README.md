babbbd4ea51fe86084de3f88ee6423a596e2116d
oezrachi <111352845+oezrachi@users.noreply.github.com>
2022-11-25 22:39:35 +0200

    1. model selection 2. refactoring 3. adding more configuration 4. more data

--- README.md
+++ README.md
@@ -5,4 +5,45 @@ Haaretz Hebrew website. I will try to build ml model that when given a headline
 from one of the two, it will classify it to it source with high probability.
 Hopefully, at list 85%.
 
-My firs
+# Data probe
+First I tried to search for resources, since I wanted to get as match sample as I can.
+I found out that both newspapers website offer RSS page that I can get into using http get request.
+Haaretz RSS page is formatted as needed, but IsraelHayom's RSS page has a lot of encoding errors.
+It took me some time to parse their xml, but I found the pattern of their errors.
+Now, the website prob function can get a normal RSS page and get the data from there.
+
+Secondly, I wanted to get all the data from beginning of the work till handout of the script.
+So I wrote a batch file that I set the Windows Task scheduler to run in each hour.
+Now I have around 1,200 sample from both newspapers.
+I'm saving the sample as json file with the classification (0 for Haaretz and 1 for IsraelHayom).
+
+# Preprocessing
+Since the problem is classifying text, after having some data, I have to convert it to a way that I will be able to 
+run Machine Learning algorithms.
+I used sklearn CountVectorizer that set a each word in the train set a number and then returns a matrix where for 
+each sample in the i'th column there is 1 iff the word that got the number i is in that sample.
+Then I used sklearn TfidfTransformer in order to normalize the cells.
+Since I wanted to save some samples that later on I will use to evaluate real prediction, I splited the sample to 
+test and train sets.
+Note that the CountVectorizer vocabulary is not fitted with the train set sample in order to get as match closer to 
+real test cases, where there may be word that won't appear in the vocabulary.
+
+# Training the model
+
+# Using the script
+The script can be run in four configurations:
+1. Update data set-Let the user update the dataset from current website RSS:
+    Run the script with 'update' as second argument of the program
+2. Load test - Let the user download from website a separate dataset to test the model on data that it has never seen 
+   before:
+    Run the script with 'load_test' as second argument of the program
+3. Fit - Let the user fit the model using the ml methods mentioned above
+    Run the script with 'fit' as second argument of the program
+    Note: There should be dataset with sample in the directory mentioned in the top of the script (The mode need to 
+   learn from something :))
+4. Predict - Let the user use a trained model to clsify newspaper titles to Haaretz or IsraelHayom
+    Run the script with 'predict' as second argument of the program and then the path to the data to predict third 
+   argument
+
+I had very nice time writing the script and I hope you will like it.
+Thanks
--- news_clasification.py
+++ news_clasification.py
@@ -1,31 +1,39 @@
 import sys
 from datetime import datetime
-
-from sklearn.discriminant_analysis import QuadraticDiscriminantAnalysis
-from sklearn.ensemble import RandomForestClassifier
+import pickle
+from sklearn.base import BaseEstimator
+from sklearn.ensemble import RandomForestClassifier, AdaBoostClassifier
 from sklearn.feature_extraction.text import CountVectorizer, TfidfTransformer
 from sklearn.model_selection import train_test_split
-from sklearn.naive_bayes import MultinomialNB,GaussianNB
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.model_selection import cross_val_score
 from sklearn.linear_model import SGDClassifier
 import urllib3
 import xmltodict
-from typing import Dict
+from typing import Dict, List, Tuple
 import json
 import numpy as np
-
-# I choose to work with the hebrew websites and not the English ons since
-# IsraelHayom.com doesn't work good with RSS
 from sklearn.neighbors import KNeighborsClassifier
 from sklearn.svm import SVC
 from sklearn.tree import DecisionTreeClassifier
 
+# I choose to work with the hebrew websites and not the English ons since
+# IsraelHayom.com doesn't work good with RSS
+
 DATA_SOURCE = {'https://www.haaretz.co.il/srv/htz---all-articles': 0,
-               # 'https://www.haaretz.com/srv/haaretz-latest-headlines': 0,
-               # 'https://www.israelhayom.com/category/news/feed/':1,
                'https://www.israelhayom.co.il/rss.xml': 1}
-DATA_PATH = "C:/Users/Ofer Ezrachi/Documents/Python Scripts/" \
-            "News_title_classification/data/dataset.json"
+CLASSIFICATIONS = ['Haaretz', 'IsraelHayom']
+DATA_PATH = "C:/HUJI/News_title_clasification/News_title_classification/data"
 UPDATE_INTERVAL = 1800
+MODELS = [("DecisionTreeClassifier", lambda x: DecisionTreeClassifier(max_depth=x), range(1, 10)),
+          ("RandomForestClassifier", lambda x: RandomForestClassifier(max_depth=x, n_estimators=10), range(1, 10)),
+          ("SGDClassifier", lambda x: SGDClassifier(loss='hinge', penalty='l2',
+                                                    alpha=x, random_state=42), [i * 1e-3 for i in range(2, 4)]),
+          ("KNeighborsClassifier", lambda x: KNeighborsClassifier(x), range(3, 5)),
+          ("SVC liner", lambda x: SVC(kernel="linear", C=x), [i * 0.125 for i in range(1, 8)]),
+          ("AdaBoostClassifier", lambda x: AdaBoostClassifier(), [0]),
+          ("MultinomialNB", lambda x: MultinomialNB(), [0]),
+          ("SVC", lambda x: SVC(gamma=x, C=1), range(1, 8))]
 
 
 def probe_website(url: str, classification: int) -> Dict[str, int]:
@@ -66,8 +74,11 @@ def read_dump(path: str) -> Dict[str, int]:
     :param path: the path of the json file
     :return: Dictionary of the titles to their classifications
     """
-    with open(path, encoding='utf8') as file:
-        return json.load(file)
+    try:
+        with open(path, encoding='utf8') as file:
+            return json.load(file)
+    except:
+        return dict()
 
 
 def update_dataset(path: str, resources: Dict[str, int]) -> None:
@@ -96,39 +107,151 @@ def read_data(path: str) -> np.array:
     return np.array([list(data.keys()), list(data.values())])
 
 
-def check_model_succses(X_train, y_train, baseModel, X_test, y_test):
-    model = baseModel.fit(X_train, y_train)
-    predicted = model.predict(X_test)
-    return np.mean(predicted == y_test)
-
-
-def choose_model():
-    count_vect, tfidf_transformer = CountVectorizer(), TfidfTransformer()
-    dataset = read_data(DATA_PATH).T
-    X_train, X_test, y_train, y_test = train_test_split(dataset[:, 0],
-                                                        dataset[:, 1],
-                                                        test_size=0.1,
-                                                        shuffle=False)
+def preprocessing(data: np.array, count_vect: CountVectorizer, tfidf_transformer: TfidfTransformer) -> Tuple[
+    np.array, np.array,
+    np.array, np.array]:
+    """
+    This function will preprocess the data from text to numbers in order to preform data learning
+    :param data: array of text title and classifications
+    :param count_vect: count vectorize object for mapping text to fetchers
+    :param tfidf_transformer: make the mapping more accurate
+    :return: The data splited to train and test
+    """
+    dataset = data.T
+    # split to test and train
+    X_train, X_test, y_train, y_test = train_test_split(dataset[:, 0], dataset[:, 1],
+                                                        test_size=0.1, shuffle=False)
     X_train_counts = count_vect.fit_transform(X_train)
     X_test_counts = count_vect.transform(X_test)
     X_train_tfidf = tfidf_transformer.fit_transform(X_train_counts)
     X_test_tfidf = tfidf_transformer.transform(X_test_counts)
-    print(check_model_succses(X_train_tfidf, y_train,
-                              SGDClassifier(loss='hinge', penalty='l2',
-                                            alpha=1e-3, random_state=42),
-                              # MultinomialNB(),
-                              # DecisionTreeClassifier(max_depth=5),
-                              # KNeighborsClassifier(1),
-                              # RandomForestClassifier(max_depth=5,
-                              #                        n_estimators=10,
-                              #                        max_features=1),
-                              # SVC(kernel="linear", C=0.025),
-                              # SVC(gamma=2, C=1),
-                              X_test_tfidf, y_test))
+    return X_test_tfidf, X_train_tfidf, y_test, y_train
+
+
+def find_hyper(X_train: np.array, y_train: np.array, model_init, hypers: List[float]) -> float:
+    """
+    This function will choose hyperparameter for the model
+    :param X_train: training set for the model
+    :param y_train: training set for the model
+    :param model_init: the model to find hyperparameter for
+    :param hypers: the hyperparameters to choose from
+    :return: the best hyperparameter for the model
+    """
+    scores = []
+    for p in hypers:
+        clf = model_init(p)
+        s = cross_val_score(clf, X_train, y_train, cv=10, scoring='accuracy')
+        scores.append(s.mean())
+    return hypers[np.array(scores).argmax()]
+
+
+def model_selection(X_test: np.array, X_train: np.array, y_test: np.array, y_train: np.array) -> BaseEstimator:
+    """
+    This function will select a model to predict the publisher of the news title
+    :param X_test: The test set
+    :param X_train: the train set
+    :param y_test: the test labels
+    :param y_train: the train labels
+    :return: The chosen model fitted
+    """
+    best_score = 0
+    best_model = None
+    best_model_name = ""
+    for name, model, hypers in MODELS:
+        p = find_hyper(X_train, y_train, model, hypers)
+        m = model(p)
+        m.fit(X_train, y_train)
+        train_predicted = m.predict(X_train)
+        test_predicted = m.predict(X_test)
+        test_score = np.mean(y_test == test_predicted)
+        train_score = np.mean(y_train == train_predicted)
+        if test_score > best_score:
+            best_score, best_model, best_model_name = test_score, m, name
+        print(f"{name}- Test score: {round(test_score * 100, 2)}%, Train score:"
+              f" {round(train_score * 100, 2)}%.")
+    print(f"\nWinning model is {best_model_name} with accuracy rate: {round(100 * best_score, 2)}%.")
+    return best_model
+
+
+def serialize_model(path: str, model: BaseEstimator, count_vect: CountVectorizer,
+                    transformer: TfidfTransformer) -> None:
+    """
+    This function will serialize the model for future use
+    :param path: path to save the model (must be directory)
+    :param model: the main model
+    :param count_vect: the count vectorize model
+    :param transformer: the tfidf transformer model
+    """
+    with open(path + "/model", 'wb') as file:
+        pickle.dump(model, file)
+    with open(path + "/count", 'wb') as file:
+        pickle.dump(count_vect, file)
+    with open(path + "/transformer", 'wb') as file:
+        pickle.dump(transformer, file)
+
+
+def deserialize_model(path: str) -> Tuple[BaseEstimator, CountVectorizer, TfidfTransformer]:
+    """
+    This function will deserialize the model
+    :param path: path where the serialized model is saved
+    :return: fitted model, countvectorize and tfidftransformer to transform text to number fetchers
+    """
+    model, count_vect, transformer = None, None, None
+    try:
+        with open(path + "/model", 'rb') as file:
+            model = pickle.load(file)
+        with open(path + "/count", 'rb') as file:
+            count_vect = pickle.load(file)
+        with open(path + "/transformer", 'rb') as file:
+            transformer = pickle.load(file)
+    except:
+        print(f"No model loaded, run the script with fit first in order to predict", file=sys.stderr)
+    return model, count_vect, transformer
+
+
+def fit_news_classifier(data_path: str) -> None:
+    """
+    This function will find, fit and save classifier for the news title problem
+    :param data_path: path to where the dataset is saved, and where to save the result
+    """
+    data = read_data(data_path + "/dataset.json")
+    count_vect, transformer = CountVectorizer(), TfidfTransformer()
+    X_test_tfidf, X_train_tfidf, y_test, y_train = preprocessing(data, count_vect, transformer)
+    model = model_selection(X_test_tfidf, X_train_tfidf, y_test, y_train)
+    serialize_model(data_path, model, count_vect, transformer)
+
+
+def predict(model_path: str, test_path: str) -> float:
+    """
+    This function will predict where the titles saved in the test_path file where originally published
+    :param model_path: path where the model saved
+    :param test_path: the titles to predict
+    :return: success rate of the prediction
+    """
+    model, count_vect, transformer = deserialize_model(model_path)
+    test_data = read_data(test_path).T
+    X, y = test_data[:, 0], test_data[:, 1]
+    test_count = count_vect.transform(X)
+    test_trans = transformer.transform(test_count)
+    result = model.predict(test_trans)
+    for idx, prediction in enumerate(result):
+        print(
+            f"{CLASSIFICATIONS[int(prediction)] == CLASSIFICATIONS[int(y[idx])]}: The title was classified to be "
+            f"{CLASSIFICATIONS[int(prediction)]} "
+            f"newspaper.\n and it's from {CLASSIFICATIONS[int(y[idx])]}.")
+    return float(np.mean(result == y))
 
 
 if __name__ == '__main__':
-    if len(sys.argv) > 1 and sys.argv[1] == 'update':
-        update_dataset(DATA_PATH, DATA_SOURCE)
+    if len(sys.argv) > 1:
+        if sys.argv[1] == 'update':
+            update_dataset(DATA_PATH + "/dataset.json", DATA_SOURCE)
+        if sys.argv[1] == 'fit':
+            fit_news_classifier(DATA_PATH)
+        if sys.argv[1] == 'predict' and len(sys.argv) > 2:
+            print(predict(DATA_PATH, sys.argv[2]))
+        if sys.argv[1] == 'load_test':
+            update_dataset(DATA_PATH + "/test.json", DATA_SOURCE)
     else:
-        choose_model()
+        print(f"Arguments not in the current format. \nFormat: [update\\fit\\predict] [title (only for predict)]",
+              file=sys.stderr)
